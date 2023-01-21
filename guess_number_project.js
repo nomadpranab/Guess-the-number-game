e1ae600a6ab3edcc9fc2179d1a96ca2559165cba
PRANAB PATRA <pranabpatra520@gmail.com>
2023-01-21 11:24:53 +0530

    done

---
+++ guess_number_project.js
@@ -0,0 +1,25 @@
+//guess a number between 1 and 100
+
+
+let x = Math.random() * 100;
+x = Math.floor(x);
+console.log(x)
+let y=10;
+
+function start()
+{
+    while(y>0)
+    {
+    const input=parseInt(prompt("Enter your lucky number"));
+        if(input==x)
+        {
+        document.write("You won");
+        break;
+        }
+        else{  
+        y--;
+        window.alert(`You have ${y} chances left`);
+        }
+    }
+
+}
